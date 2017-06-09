8426ce1b58f6456fe1e7737e91d30688cb1a5a77
--global <jcorrigan14@gmail.com>
2017-06-09 16:45:22 -0400

    registered Task and Comment in admin interface. Fixed timezone'

--- app/admin.py
+++ app/admin.py
@@ -1,6 +1,21 @@
+"""
+Django admin page controller-
+Right now the models are registered, but the info is not formatted properly
+    i.e. No names are shown, just 'List Object'
+"""
+
+
 from django.contrib import admin
-from app.models import List
+from app.models import *
 
 class ListAdmin(admin.ModelAdmin):
     pass
 admin.site.register(List, ListAdmin)
+
+class TaskAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Task, TaskAdmin)
+
+class CommentAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Comment, CommentAdmin)
